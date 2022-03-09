ca67ce557c298c890dec35426c9d8da9349c5e2b
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-03-09 13:40:54 -0500

    create bare minimum for server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('re-testing')
+const express = require('express')
+
+const app = express()
+const PORT = 4001
+
+app.get('/', (req, res) => {
+    res.send('Welcome To Our API')
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`listening on port ${PORT}`)
+})
+
+
